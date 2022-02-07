ce19d0aaec5205d2894375651889ef42f83495d5
Frances DeBoo <frances.deboo@gmail.com>
2022-02-07 21:31:54 +0000

    install ejs

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", () => {
   console.log("hello world");
 });
